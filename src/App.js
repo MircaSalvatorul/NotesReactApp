81040ef5d23549da991fc3b46187c8654a0a2bfa
MircaSalvatorul <mateimircea919@gmail.com>
2026-03-20 21:05:39 +0200

    Styles and code fixes
    -added lots of css
    -moved to react 6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// Notice we import Routes, not Switch
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Notes from "./pages/Notes";
 
-function App() {
+export default function BasicExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {}
+      <Routes>
+        <Route path="/" element={<Notes />} /> 
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
